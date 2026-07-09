9de670582da7e2456721ab8296aab0650034392f
Kartikey Jindal <142749178+Kartikeyjindal@users.noreply.github.com>
2026-07-10 01:46:38 +0530

    Add java solution for LeetCode: Count Nodes Equal To Average Of Subtree

--- count-nodes-equal-to-average-of-subtree/Solution.java
+++ count-nodes-equal-to-average-of-subtree/Solution.java
@@ -1,31 +1,64 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution 
 {
-    int count = 0;
-
+    int count=0;
     public int averageOfSubtree(TreeNode root) 
     {
         dfs(root);
         return count;
     }
 
-    public int[] dfs(TreeNode root)
+    public void dfs(TreeNode root)
     {
-        if (root == null)
+        if(root==null)
         {
-            return new int[]{0, 0}; // sum, count
+            return ;
         }
-
-        int[] left = dfs(root.left);
-        int[] right = dfs(root.right);
-
-        int sum = left[0] + right[0] + root.val;
-        int num = left[1] + right[1] + 1;
-
-        if (sum / num == root.val)
+        dfs(root.left);
+        dfs(root.right);
+        int sum=sumsolve(root);
+        int num=numsolve(root);
+        if(sum/num==root.val)
         {
             count++;
         }
-
-        return new int[]{sum, num};
     }
+
+
+    public int sumsolve(TreeNode root)
+    {
+        if(root==null)
+        {
+            return 0;
+        }
+        int left=sumsolve(root.left);
+        int right=sumsolve(root.right);
+        return root.val+left+right;
+    } 
+
+    public int numsolve(TreeNode root)
+    {
+        if(root==null)
+        {
+            return 0;
+        }
+        int left=numsolve(root.left);
+        int right=numsolve(root.right);
+        return 1+left+right;
+    }
+
 }
