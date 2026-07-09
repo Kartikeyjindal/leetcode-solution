946d9a3ae52e2a2b8f12338327efcbeb27aa74d9
Kartikey Jindal <142749178+Kartikeyjindal@users.noreply.github.com>
2026-07-10 02:03:28 +0530

    Add java solution for LeetCode: Minimum Path Sum

---
+++ minimum-path-sum/Solution.java
@@ -0,0 +1,31 @@
+class Solution 
+{
+    public int minPathSum(int[][] grid) 
+    {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+
+        for (int row = 1; row < m; row++)
+        {
+            dp[row][0] = grid[row][0] + dp[row-1][0];
+        }
+
+        for (int col = 1; col < n; col++)
+        {
+            dp[0][col] = grid[0][col] + dp[0][col-1];
+        }
+
+        for (int i = 1; i < m; i++)
+        {
+            for (int j = 1; j < n; j++)
+            {
+                dp[i][j] = grid[i][j] + Math.min(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+
+        return dp[m-1][n-1];
+    }
+}
