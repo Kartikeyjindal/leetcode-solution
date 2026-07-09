1cee3c0f6cba7fbd0289cf6657f49b9a10a0c36d
Kartikey Jindal <142749178+Kartikeyjindal@users.noreply.github.com>
2026-07-10 01:46:44 +0530

    Add java solution for LeetCode: Find Largest Value In Each Tree Row

---
+++ find-largest-value-in-each-tree-row/Solution.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution 
+{
+    public List<Integer> largestValues(TreeNode root) 
+    {
+        List<Integer> ans=new ArrayList<>();
+        Queue<TreeNode> queue=new LinkedList<>();
+        if(root==null)
+        {
+            return ans;
+        }
+        queue.add(root);
+        int maxv;
+        while(!queue.isEmpty())
+        {
+            int size=queue.size();
+            maxv=Integer.MIN_VALUE;
+            for(int i=0;i<size;i++)
+            {
+                TreeNode node=queue.poll();
+                maxv=Math.max(maxv,node.val);
+                if(node.left!=null)
+                {
+                    queue.add(node.left);
+                }
+                if(node.right!=null)
+                {
+                    queue.add(node.right);
+                }
+            }
+            ans.add(maxv);
+        }
+        return ans;
+    }
+}
