a519f9f3bf4b5a346e856b0a1c9ffa91e8813303
Kartikey Jindal <142749178+Kartikeyjindal@users.noreply.github.com>
2026-07-10 02:01:36 +0530

    Add java solution for LeetCode: Cherry Pickup

--- cherry-pickup/Solution.java
+++ cherry-pickup/Solution.java
@@ -1,36 +1,33 @@
 class Solution {
+    int[][][][] dp;
     int n, m;
-    int[][][] dp;
 
     public int cherryPickup(int[][] grid) {
         n = grid.length;
         m = grid[0].length;
-        dp = new int[n][m][n];
-
-        for (int[][] arr2d : dp) {
-            for (int[] arr1d : arr2d) {
-                Arrays.fill(arr1d, Integer.MIN_VALUE);
+        dp = new int[n][m][n][m];
+        
+        for (int[][][] a : dp) {
+            for (int[][] b : a) {
+                for (int[] c : b) {
+                    Arrays.fill(c, Integer.MIN_VALUE);
+                }
             }
         }
-
-        return Math.max(0, solve(0, 0, 0, grid));
+        return Math.max(0, solve(0, 0, 0, 0, grid));
     }
 
-    private int solve(int r1, int c1, int r2, int[][] grid) {
-        int c2 = r1 + c1 - r2;
-
-        if (r1 >= n || c1 >= m || r2 >= n || c2 >= m || 
-            grid[r1][c1] == -1 || grid[r2][c2] == -1) {
+    private int solve(int r1, int c1, int r2, int c2, int[][] grid) {
+        if (r1 >= n || c1 >= m || r2 >= n || c2 >= m 
+            || grid[r1][c1] == -1 || grid[r2][c2] == -1) {
             return Integer.MIN_VALUE;
         }
-
-
         if (r1 == n - 1 && c1 == m - 1) {
             return grid[r1][c1];
         }
 
-        if (dp[r1][c1][r2] != Integer.MIN_VALUE) {
-            return dp[r1][c1][r2];
+        if (dp[r1][c1][r2][c2] != Integer.MIN_VALUE) {
+            return dp[r1][c1][r2][c2];
         }
 
         int cherries = 0;
@@ -41,12 +38,12 @@ class Solution {
         }
 
         int best = Math.max(
-            Math.max(solve(r1, c1 + 1, r2, grid), solve(r1 + 1, c1, r2 + 1, grid)),
-            Math.max(solve(r1 + 1, c1, r2, grid), solve(r1, c1 + 1, r2 + 1, grid))
+            Math.max(solve(r1, c1 + 1, r2, c2 + 1, grid), solve(r1 + 1, c1, r2 + 1, c2, grid)),
+            Math.max(solve(r1 + 1, c1, r2, c2 + 1, grid), solve(r1, c1 + 1, r2 + 1, c2, grid))
         );
 
         cherries += best;
-        dp[r1][c1][r2] = cherries;
+        dp[r1][c1][r2][c2] = cherries;
         return cherries;
     }
 }
