01f15592fbb256aa74ddabe94488e9ce24051acc
Kartikey Jindal <142749178+Kartikeyjindal@users.noreply.github.com>
2026-07-10 01:56:41 +0530

    Add java solution for LeetCode: Binary Tree Right Side View

--- binary-tree-right-side-view/Solution.java
+++ binary-tree-right-side-view/Solution.java
@@ -1,23 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution 
 {
     public List<Integer> rightSideView(TreeNode root) 
     {
-        List<Integer> list = new ArrayList<>();
-        preorder(root, 1, list);
+        List<Integer> list=new ArrayList<>();
+        if (root == null) return list;
+        Queue<TreeNode> queue=new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty())
+        {
+            int n=queue.size();
+            TreeNode node=null;
+            while(n-- >0)
+            {
+                node=queue.poll();
+                if(node.left!=null)
+                {
+                    queue.add(node.left);
+                }
+                if(node.right!=null)
+                {
+                    queue.add(node.right);
+                }
+            }
+            list.add(node.val);
+        }
         return list;
-    }
-
-    public void preorder(TreeNode root, int level, List<Integer> list)
-    {
-        if (root == null)
-        {
-            return;
-        }
-        if (list.size() < level)
-        {
-            list.add(root.val);
-        }
-        preorder(root.right, level + 1, list);
-        preorder(root.left, level + 1, list);
     }
 }
