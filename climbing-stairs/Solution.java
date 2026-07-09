a7c5a9bcd6e6ab699839ecf8ee3bfdb1df4e0447
Kartikey Jindal <142749178+Kartikeyjindal@users.noreply.github.com>
2026-07-10 02:08:56 +0530

    Add java solution for LeetCode: Climbing Stairs

--- climbing-stairs/Solution.java
+++ climbing-stairs/Solution.java
@@ -2,19 +2,18 @@ class Solution
 {
     public int climbStairs(int n) 
     {
-        if(n==1 || n==2 || n==3)
+       if(n==1 || n==2 || n==3)
        {
         return n;
        }
-        int []t=new int[n+1];
-        t[0]=0;
-        t[1]=1;
-        t[2]=2;
-
-        for(int i=3;i<=n;i++)
-        {
-            t[i]=t[i-1]+t[i-2];
-        }
-        return t[n];
+       int []t=new int[n+1];
+       t[0]=0;
+       t[1]=1;
+       t[2]=2;
+       for(int i=3;i<=n;i++)
+       {
+        t[i]=t[i-1]+t[i-2];
+       }
+       return t[n];
     }
 }
