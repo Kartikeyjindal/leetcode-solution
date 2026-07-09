594f43276f703ca531c08d92d28b12d5c883823d
Kartikey Jindal <142749178+Kartikeyjindal@users.noreply.github.com>
2026-07-10 02:03:01 +0530

    Add java solution for LeetCode: Jump Game Ii

--- jump-game-ii/Solution.java
+++ jump-game-ii/Solution.java
@@ -1,22 +1,33 @@
 class Solution {
     int[] dp;
 
-    public int jump(int[] nums) 
+    public int jump(int[] nums) {
+        int n = nums.length;
+        dp = new int[n];
+        Arrays.fill(dp, -1);
+        return solve(0, nums);
+    }
+
+    public int solve(int index, int[] nums) 
     {
-        int l=0,r=0;
-        int n=nums.length;
-        int jump=0,farthest;
-        while(r<n-1)
+        if (index >= nums.length - 1) return 0;
+        if (dp[index] != -1) return dp[index];
+
+        int minjumps = Integer.MAX_VALUE;
+
+        for (int step = 1; step <= nums[index]; step++) 
         {
-            farthest=0;
-            for(int i=l;i<=r;i++)
+            int nextindex = index + step;
+            if (nextindex < nums.length) 
             {
-                farthest=Math.max(farthest,i+nums[i]);
+                int result = solve(nextindex, nums);
+                if (result != Integer.MAX_VALUE) 
+                {
+                    minjumps = Math.min(minjumps, 1 + result);
+                }
             }
-            l=r+1;
-            r=farthest;
-            jump++;
         }
-        return jump;
+
+        return dp[index] = minjumps;
     }
 }
