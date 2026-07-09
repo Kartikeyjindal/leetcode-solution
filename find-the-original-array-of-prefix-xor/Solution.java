f707ea5e58f8335fff0a44d96509f5e86cbe4a30
Kartikey Jindal <142749178+Kartikeyjindal@users.noreply.github.com>
2026-07-10 01:38:03 +0530

    Add java solution for LeetCode: Find The Original Array Of Prefix Xor

---
+++ find-the-original-array-of-prefix-xor/Solution.java
@@ -0,0 +1,18 @@
+class Solution 
+{
+    public int[] findArray(int[] pref) 
+    {
+        int n=pref.length;
+        int[]ans=new int[n];
+        if(n==1)
+        {
+            return pref;
+        }
+        ans[0]=pref[0];
+        for(int i=1;i<n;i++)
+        {
+            ans[i]=pref[i]^pref[i-1];
+        }
+        return ans;
+    }
+}
