837639415d430a0d83cae46a870e348c9fbf452b
Kartikey Jindal <142749178+Kartikeyjindal@users.noreply.github.com>
2026-07-10 01:19:28 +0530

    Add java solution for LeetCode: Find The Middle Index In Array

---
+++ find-the-middle-index-in-array/Solution.java
@@ -0,0 +1,23 @@
+class Solution 
+{
+    public int findMiddleIndex(int[] nums) 
+    {
+        int n=nums.length;
+        int sum=0;
+        for(int i:nums)
+        {
+            sum+=i;
+        }
+        int currsum=0;
+        for(int i=0;i<n;i++)
+        {
+            if(sum-nums[i]-currsum==currsum)
+            {
+                return i;
+            }
+            currsum+=nums[i];
+
+        }
+        return -1;
+    }
+}
