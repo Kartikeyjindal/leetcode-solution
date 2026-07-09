207e4cc42ebb6ca5fd7ee743fea6a656dda43c4b
Kartikey Jindal <142749178+Kartikeyjindal@users.noreply.github.com>
2026-07-10 01:47:46 +0530

    Add java solution for LeetCode: Maximum Width Of Binary Tree

--- maximum-width-of-binary-tree/Solution.java
+++ maximum-width-of-binary-tree/Solution.java
@@ -1,33 +1,48 @@
-class Solution {
-    public int widthOfBinaryTree(TreeNode root) {
-        if (root == null) return 0;
-        
-        Queue<Pair<TreeNode, Integer>> queue = new LinkedList<>();
-        queue.add(new Pair<>(root, 0));
-        int maxWidth = 0;
-        
-        while (!queue.isEmpty()) {
-            int levelLength = queue.size();
-            int levelStart = queue.peek().getValue();
-            int index = 0;
-            
-            for (int i = 0; i < levelLength; i++) {
-                Pair<TreeNode, Integer> pair = queue.poll();
-                TreeNode node = pair.getKey();
-                index = pair.getValue();
-                
-                if (node.left != null) {
-                    queue.add(new Pair<>(node.left, 2*index));
-                }
-                
-                if (node.right != null) {
-                    queue.add(new Pair<>(node.right, 2*index+1));
-                }
-            }
-            
-            maxWidth = Math.max(maxWidth, index - levelStart + 1);
+class Solution 
+{
+    class Pair 
+    {
+        TreeNode node;
+        long idx;
+        Pair(TreeNode n, long i) 
+        {
+            node = n;
+            idx = i;
         }
-        
-        return maxWidth;
+    }
+
+    public int widthOfBinaryTree(TreeNode root) 
+    {
+        if (root == null) return 0;
+
+        Queue<Pair> queue = new LinkedList<>();
+        queue.add(new Pair(root, 0L));
+        long maxw = 0;
+
+        while (!queue.isEmpty())
+        {
+            int size = queue.size();
+            long start = queue.peek().idx;
+            long end = start;
+
+            for (int i = 0; i < size; i++)
+            {
+                Pair p = queue.poll();
+                TreeNode node = p.node;
+                long idx = p.idx;
+
+                end = idx;
+
+                if (node.left != null)
+                    queue.add(new Pair(node.left, 2 * idx));
+
+                if (node.right != null)
+                    queue.add(new Pair(node.right, 2 * idx + 1));
+            }
+
+            maxw = Math.max(maxw, end - start + 1);
+        }
+
+        return (int) maxw;
     }
 }
