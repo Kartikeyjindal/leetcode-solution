8599d3ba1cef84104d6e972201681099757201a2
Kartikey Jindal <142749178+Kartikeyjindal@users.noreply.github.com>
2026-07-10 01:30:57 +0530

    Add java solution for LeetCode: Evaluate Reverse Polish Notation

---
+++ evaluate-reverse-polish-notation/Solution.java
@@ -0,0 +1,54 @@
+// class pair
+// {
+//     int a;int b;
+//     pair(int a,int b)
+//     {
+//         this.a=a;
+//         this.b=b;
+//     }
+// }
+class Solution 
+{
+    public int evalRPN(String[] tokens) 
+    {
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for(String s:tokens)
+        {
+            if(s.equals("+"))
+            {
+                int a=stack.pop();
+                int b=stack.pop();
+
+                stack.push(a+b);
+            }
+            else if(s.equals("-"))
+            {
+                int a=stack.pop();
+                int b=stack.pop();
+
+                stack.push(b-a);
+            }
+            else if(s.equals("*"))
+            {
+                int a=stack.pop();
+                int b=stack.pop();
+
+                stack.push(a*b);
+            }
+            else if(s.equals("/"))
+            {
+                int a=stack.pop();
+                int b=stack.pop();
+                int div=b/a;
+                stack.push(div);
+            }
+            else if(s.matches("^-?\\d+"))
+            {
+                stack.push(Integer.parseInt(s));
+            }
+        }
+        return stack.pop();
+
+    }
+}
