a8f77f689ec96ef7a1109645bf6e9cee9078ca9e
Kartikey Jindal <142749178+Kartikeyjindal@users.noreply.github.com>
2026-07-10 01:59:30 +0530

    Add java solution for LeetCode: Pascals Triangle

---
+++ pascals-triangle/Solution.java
@@ -0,0 +1,25 @@
+class Solution 
+{
+    public List<List<Integer>> generate(int numRows) 
+    {
+        List<List<Integer>> triangle=new ArrayList<List<Integer>>();
+        triangle.add(new ArrayList<>());
+        triangle.get(0).add(1);
+
+        for(int i=1;i<numRows;i++)
+        {
+            List<Integer> row=new ArrayList<>();
+            List<Integer> prevrow=triangle.get(i-1);
+
+            row.add(1);
+            for(int j=1;j<i;j++)
+            {
+                row.add(prevrow.get(j-1)+prevrow.get(j));
+            }
+            row.add(1);
+
+            triangle.add(row);
+        }
+        return triangle;
+    }
+}
