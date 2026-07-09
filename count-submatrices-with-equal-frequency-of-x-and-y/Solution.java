0c34722e320df4ff12cf2c8680c83c9a0f09f7c9
Kartikey Jindal <142749178+Kartikeyjindal@users.noreply.github.com>
2026-07-10 01:35:33 +0530

    Add java solution for LeetCode: Count Submatrices With Equal Frequency Of X And Y

--- count-submatrices-with-equal-frequency-of-x-and-y/Solution.java
+++ count-submatrices-with-equal-frequency-of-x-and-y/Solution.java
@@ -1,89 +1,66 @@
+class pair
+{
+    int a;
+    int b;
+    pair(int a,int b)
+    {
+        this.a=a;
+        this.b=b;
+    }
+}
 class Solution 
 {
     public int numberOfSubmatrices(char[][] grid) 
     {
-        int nx=0;
-        int ny=0;
         int n=grid.length;
         int m=grid[0].length;
-        int[][]g=new int[n][m];
+        pair[][]p=new pair[n][m];
+
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 if(grid[i][j]=='X')
                 {
-                    g[i][j]=1;
-                    nx++;
+                    p[i][j]= new pair(1,0);
                 }
                 else if(grid[i][j]=='Y')
                 {
-                    g[i][j]=-1;
-                    ny++;
+                    p[i][j]= new pair(0,1);
                 }
                 else
                 {
-                    g[i][j]=0;
+                    p[i][j]= new pair(0,0);
                 }
-            }
-        }
-        if(nx==0 || ny==0)
-        {
-            return 0;
-        }
-        int [][]prefix=new int[n][m];
-        prefix[0][0]=g[0][0];
-
-        for(int i=1;i<n;i++)
-        {
-            prefix[i][0]=prefix[i-1][0]+g[i][0];
-        }
-        for(int i=1;i<m;i++)
-        {
-            prefix[0][i]=prefix[0][i-1]+g[0][i];
-        }
-
-        for(int i=1;i<n;i++)
-        {
-            for(int j=1;j<m;j++)
-            {
-                prefix[i][j]=prefix[i-1][j]+prefix[i][j-1]-prefix[i-1][j-1]+g[i][j];
-            }
-        }
-        int ans=0;
-        int idx=n;
-        int sub=0;
-        for(int j=0; j<m; j++)
-        {
-            if(prefix[0][j]!=0)
-            {
-                idx = j;
-                break;
-            }
-            else sub++;
-        }
-        for(int i=1; i<n; i++)
-        {
-            for(int j=0; j<idx; j++)
-            {
-                if(prefix[i][j]!=0)
-                {
-                    idx = j;
-                    break;
-                }
-                else sub++;
             }
         }
         for(int i=0;i<n;i++)
         {
+            for(int j=1;j<m;j++)
+            {
+                p[i][j].a+=p[i][j-1].a;
+                p[i][j].b+=p[i][j-1].b;
+            }
+        }
+        for(int i=1;i<n;i++)
+        {
             for(int j=0;j<m;j++)
             {
-                if(prefix[i][j]==0)
+                p[i][j].a+=p[i-1][j].a;
+                p[i][j].b+=p[i-1][j].b;
+            }
+        }
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(p[i][j].a>0 && p[i][j].b==p[i][j].a)
                 {
                     ans++;
                 }
             }
         }
-        return ans-sub;
+        return ans;
     }
 }
