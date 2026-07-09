7d31a1c05eccbbe462067565e3efd7490cb77979
Kartikey Jindal <142749178+Kartikeyjindal@users.noreply.github.com>
2026-07-10 01:48:04 +0530

    Add java solution for LeetCode: Sum Root To Leaf Numbers

---
+++ sum-root-to-leaf-numbers/Solution.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution 
+{
+    public int sumNumbers(TreeNode root) 
+    {
+        return solve(root,0);
+    }
+    public int solve(TreeNode root,int currsum)
+    {
+        if(root==null)
+        {
+            return 0;
+        }
+        currsum=currsum*10+root.val;
+        if(root.left==null && root.right==null)
+        {
+            return currsum;
+        }
+        int left=solve(root.left,currsum);
+        int right=solve(root.right,currsum);
+
+        return left+right;
+    }
+}
