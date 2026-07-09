44fb78458d4bd66958154c912958e0416c7be6c9
Kartikey Jindal <142749178+Kartikeyjindal@users.noreply.github.com>
2026-07-10 01:58:16 +0530

    Add java solution for LeetCode: Valid Triangle Number

--- valid-triangle-number/Solution.java
+++ valid-triangle-number/Solution.java
@@ -1,13 +1,21 @@
-public class Solution {
+class Solution {
     public int triangleNumber(int[] nums) {
+        int n = nums.length;
+        if(n < 3) return 0;
         int count = 0;
         Arrays.sort(nums);
-        for (int i = 0; i < nums.length - 2; i++) {
-            int k = i + 2;
-            for (int j = i + 1; j < nums.length-1 && nums[i] != 0; j++) {
-                while (k < nums.length && nums[i] + nums[j] > nums[k])
-                    k++;
-                count += k - j - 1;
+
+        for(int k = n-1; k > 1; k--){
+            int i = 0;
+            int j = k-1;
+
+            while(i < j){
+                if(nums[i] + nums[j] > nums[k]){
+                    count += (j-i);
+                    j--;
+                }else{
+                    i++;
+                }
             }
         }
         return count;
