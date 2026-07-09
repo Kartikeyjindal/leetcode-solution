57feeb13e624b07a20da7f2a25aa7f02b8cfbb6c
Kartikey Jindal <142749178+Kartikeyjindal@users.noreply.github.com>
2026-07-10 02:00:02 +0530

    Add java solution for LeetCode: Dungeon Game

---
+++ dungeon-game/Solution.java
@@ -0,0 +1,33 @@
+class Solution 
+{
+    public int calculateMinimumHP(int[][] dungeon) 
+    {
+        int m=dungeon.length;
+        int n=dungeon[0].length;
+        int dp[][]=new int[m][n];
+        for(int []nums:dp)
+        {
+            Arrays.fill(nums,-1);
+        }
+        return solve(0,0,m,n,dungeon,dp);
+    }
+    public int solve(int i,int j,int m,int n,int[][]dun,int [][]dp)
+    {
+        if(i>=m||j>=n)
+        {
+            return Integer.MAX_VALUE;
+        }
+        if(i==m-1&&j==n-1)
+        {
+            return Math.max(1,1-dun[i][j]);
+        }
+        if(dp[i][j]!=-1)
+        {
+            return dp[i][j];
+        }
+        int right=solve(i,j+1,m,n,dun,dp);
+        int down=solve(i+1,j,m,n,dun,dp);
+        int total=Math.min(right,down)-dun[i][j];
+        return dp[i][j]=Math.max(1,total);
+    }
+}
