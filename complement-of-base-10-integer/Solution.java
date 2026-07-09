181de499dfb2611b0734696a582cd6909aad6693
Kartikey Jindal <142749178+Kartikeyjindal@users.noreply.github.com>
2026-07-10 01:36:30 +0530

    Add java solution for LeetCode: Complement Of Base 10 Integer

--- complement-of-base-10-integer/Solution.java
+++ complement-of-base-10-integer/Solution.java
@@ -2,21 +2,22 @@ class Solution
 {
     public int bitwiseComplement(int n) 
     {
-        String binary=Integer.toBinaryString(n);
-
-        StringBuilder sb=new StringBuilder();
-
-        for(char ch:binary.toCharArray())
-        {
-            if(ch=='1')
+       if(n==0)
+       {
+        return 1;
+       } 
+       int ans=0;
+       int i=0;
+       while(n>0)
+       {
+            int bit=n%2;
+            if(bit==0)
             {
-                sb.append('0');
+                ans+=Math.pow(2,i);
             }
-            else
-            {
-                sb.append('1');
-            }
-        } 
-        return Integer.parseInt(sb.toString(),2);
+            i++;
+            n=n/2;
+       }
+       return ans;
     }
 }
