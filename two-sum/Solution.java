0bc409848c5f16688fa82bbe8e2579da3d91d012
Kartikey Jindal <142749178+Kartikeyjindal@users.noreply.github.com>
2026-07-10 01:28:11 +0530

    Add java solution for LeetCode: Two Sum

--- two-sum/Solution.java
+++ two-sum/Solution.java
@@ -1,18 +1,16 @@
-class Solution 
-{
-    public int[] twoSum(int[] nums, int target) 
-    {
-        int n=nums.length;
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<n;i++)
+class Solution {
+    public int[] twoSum(int[] nums, int target)
+     {
+        for (int i = 0; i < nums.length; i++) 
         {
-            int find=target-nums[i];
-            if(map.containsKey(find))
+            for (int j = i + 1; j < nums.length; j++) 
             {
-                return new int[]{map.get(find),i};
+                if (nums[i] + nums[j] == target) 
+                {
+                    return new int[] {i, j};
+                }
             }
-            map.put(nums[i],i);
-        } 
-        return new int[]{};
+        }
+        return new int[] {};
     }
 }
