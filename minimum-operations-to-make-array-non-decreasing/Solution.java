f5ca2af20939f577ef07f69d2db40585b41ca9e4
Kartikey Jindal <142749178+Kartikeyjindal@users.noreply.github.com>
2026-07-10 01:28:55 +0530

    Add java solution for LeetCode: Minimum Operations To Make Array Non Decreasing

---
+++ minimum-operations-to-make-array-non-decreasing/Solution.java
@@ -0,0 +1,21 @@
+class Solution 
+{
+    public long minOperations(int[] nums) 
+    {
+        int n=nums.length;
+        if(n<=1)
+        {
+            return 0;
+        }
+        long ans=0;
+        for(int i=1;i<n;i++)
+            {
+                if(nums[i]<nums[i-1])
+                {
+                    int diff=nums[i-1]-nums[i];
+                    ans+=diff;
+                }
+            }
+        return ans;
+    }
+}
