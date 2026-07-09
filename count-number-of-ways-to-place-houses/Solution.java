c19d583dffb0a985b21752d26f738af00d482a39
Kartikey Jindal <142749178+Kartikeyjindal@users.noreply.github.com>
2026-07-10 02:02:34 +0530

    Add java solution for LeetCode: Count Number Of Ways To Place Houses

--- count-number-of-ways-to-place-houses/Solution.java
+++ count-number-of-ways-to-place-houses/Solution.java
@@ -2,21 +2,29 @@ class Solution
 {
     int []dp=new int[10001];
     final int MOD = 1_000_000_007;
-
     public int countHousePlacements(int n) 
     {
         Arrays.fill(dp, -1);
-
-        int oneside = solve(n);
-        long ans = (1L * oneside * oneside) % MOD;
-        return (int) ans;
+        int ways=solve(n);
+        long ans=(long)ways*ways;
+        return (int)(ans%MOD);
     }
-    public int solve(int i)
-    {
-        if(i == 0) return 1;
-        if(i == 1) return 2;
 
-        if(dp[i] != -1) return dp[i];
-        return dp[i] = (solve(i - 1) + solve(i - 2)) % MOD;
+    public int solve(int n)
+    {
+        if(n==0)
+        {
+            return dp[n]=1;
+        }
+        if(n==1)
+        {
+            return dp[n]=2;
+        }
+        if(dp[n]!=-1)
+        {
+            return dp[n];
+        }
+        return dp[n]=(solve(n-1)+solve(n-2))%MOD;
+
     }
 }
