bab7a31077dd6c04ac590ef01acc23cd6294dbda
Kartikey Jindal <142749178+Kartikeyjindal@users.noreply.github.com>
2026-07-10 01:11:51 +0530

    Add java solution for LeetCode: Maximum Alternating Subsequence Sum

--- maximum-alternating-subsequence-sum/Solution.java
+++ maximum-alternating-subsequence-sum/Solution.java
@@ -1,17 +1,40 @@
-class Solution
+class Solution 
 {
+    long [][]dp;
     public long maxAlternatingSum(int[] nums) 
     {
         int n=nums.length;
-        long [][]dp=new long[n+1][2];
-        int even=0;
-        int odd=1;
-
-        for(int i=1;i<=n;i++)
+        dp=new long[nums.length][2];
+        for(int i=0;i<n;i++)
         {
-            dp[i][even]=Math.max(dp[i-1][odd]+nums[i-1],dp[i-1][even]);
-            dp[i][odd]=Math.max(dp[i-1][even]-nums[i-1],dp[i-1][odd]);
+            Arrays.fill(dp[i],-1);
         }
-        return Math.max(dp[n][odd],dp[n][even]);
+        return solve(0,1,nums);
+    }
+
+    public long solve(int i,int flag ,int[]nums)
+    {
+        if(i>=nums.length)
+        {
+            return 0;
+        }
+        if(dp[i][flag]!=-1)
+        {
+            return dp[i][flag];
+        }
+        long skip=solve(i+1,flag,nums);
+        long pick=0;
+
+        if(flag==1)
+        {
+            pick=nums[i]+solve(i+1,0,nums);
+        }
+        else
+        {
+            pick=-nums[i]+solve(i+1,1,nums);
+        }
+
+        return dp[i][flag]=Math.max(skip,pick);
+
     }
 }
