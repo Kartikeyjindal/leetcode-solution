723121adc45397930eee62cb9359b99ed8cfa6a9
Kartikey Jindal <142749178+Kartikeyjindal@users.noreply.github.com>
2026-07-10 01:25:02 +0530

    Add java solution for LeetCode: Group Anagrams

--- group-anagrams/Solution.java
+++ group-anagrams/Solution.java
@@ -3,42 +3,25 @@ class Solution
     public List<List<String>> groupAnagrams(String[] strs) 
     {
         List<List<String>> ans=new ArrayList<>();
-        Map<String,List<String>> map=new HashMap<>();
+        Map<String ,List<String>> map=new HashMap<>();
 
         for(String s:strs)
         {
-            String generated=solve(s);
-            if(!map.containsKey(generated))
+            char []sort=s.toCharArray();
+            Arrays.sort(sort);
+            String sorted=new String(sort);
+            if(!map.containsKey(sorted))
             {
-                map.put(generated,new ArrayList<>());
+                map.put(sorted, new ArrayList<>());
             }
-            map.get(generated).add(s);
+            map.get(sorted).add(s);
         }
 
         for(Map.Entry<String,List<String>> entry:map.entrySet())
         {
-            ans.add(entry.getValue());
+            List<String> a=entry.getValue();
+            ans.add(a);
         }
         return ans;
-    }
-
-    public String solve(String str)
-    {
-        int[] alp=new int[26];
-
-        for(char c:str.toCharArray())
-        {
-            alp[c-'a']++;
-        }
-        StringBuilder s=new StringBuilder();
-        for(int i=0;i<26;i++)
-        {
-            while(alp[i]>0)
-            {
-                s.append((char)('a'+i));
-                alp[i]--;
-            }
-        }
-        return s.toString();
     }
 }
