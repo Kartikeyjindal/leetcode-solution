aa101446335967cd1322cebd7074218d5eb2e56f
Kartikey Jindal <142749178+Kartikeyjindal@users.noreply.github.com>
2026-07-10 02:06:07 +0530

    Add java solution for LeetCode: Longest String Chain

--- longest-string-chain/Solution.java
+++ longest-string-chain/Solution.java
@@ -1,7 +1,7 @@
-class Solution 
-{
-    public boolean predecessor(String pre, String cur) 
-    {
+class Solution {
+    public int[][] dp = new int[1001][1001];
+
+    public boolean predecessor(String pre, String cur) {
         int m = pre.length();
         int n = cur.length();
         if (m >= n || n - m != 1) {
@@ -17,25 +17,36 @@ class Solution
         return i == m;
     }
 
-    public int longestStrChain(String[] words) 
-    {
-        int n=words.length;
-        Arrays.sort(words, (a, b) -> Integer.compare(a.length(), b.length()));
-        int []dp=new int[n];
-        Arrays.fill(dp,1);
-
-        int maxl=1;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<i;j++)
-            {
-                if(predecessor(words[j],words[i]))
-                {
-                    dp[i]=Math.max(dp[i],dp[j]+1);
-                    maxl=Math.max(maxl,dp[i]);
-                }
-            }
+    public int longestStrChain(String[] words) {
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
         }
-        return maxl;
+
+        Arrays.sort(words, (a, b) -> Integer.compare(a.length(), b.length()));
+        return lis(words, -1, 0);
+    }
+
+    public int lis(String[] words, int prev, int curr) {
+        if (curr == words.length) {
+            return 0;
+        }
+
+        if (prev != -1 && dp[prev + 1][curr] != -1) {
+            return dp[prev + 1][curr];
+        }
+
+        int take = 0;
+        if (prev == -1 || predecessor(words[prev], words[curr])) {
+            take = 1 + lis(words, curr, curr + 1);
+        }
+
+        int skip = lis(words, prev, curr + 1);
+        int result = Math.max(take, skip);
+
+        if (prev != -1) {
+            dp[prev + 1][curr] = result;
+        }
+
+        return result;
     }
 }
