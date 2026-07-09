a0344d75df23d2c9976609dda6eebe2337cd27e0
Kartikey Jindal <142749178+Kartikeyjindal@users.noreply.github.com>
2026-07-10 02:00:37 +0530

    Add java solution for LeetCode: 3sum

--- 3sum/Solution.java
+++ 3sum/Solution.java
@@ -2,50 +2,40 @@ class Solution
 {
     public List<List<Integer>> threeSum(int[] nums) 
     {
-        int n=nums.length;
-        Arrays.sort(nums);
-        int target;
         List<List<Integer>> ans=new ArrayList<>();
+        Arrays.sort(nums);
 
-        for(int i=0;i<n;i++)
+        for(int i=0;i<nums.length;i++)
         {
             if(i>0 && nums[i]==nums[i-1])
             {
                 continue;
             }
-            target=-nums[i];
-            twosum(target,i+1,n-1,ans,nums,i);
+            int j=i+1;
+            int k=nums.length-1;
+            while(j<k)
+            {
+                int total=nums[i]+nums[j]+nums[k];
+                if(total>0)
+                {
+                    k--;
+                }
+                else if(total<0)
+                {
+                    j++;
+                }
+                else
+                {
+                    ans.add(Arrays.asList(nums[i],nums[k],nums[j]));
+                    j++;
+
+                    while(nums[j]==nums[j-1] && j<k)
+                    {
+                        j++;
+                    }
+                }
+            }
         }
         return ans;
-    }
-
-    public void twosum(int target,int l,int r,List<List<Integer>> ans,int []nums,int i)
-    {
-        while(l<r)
-        {
-            int sum=nums[l]+nums[r];
-            if(sum>target)
-            {
-                r--;
-            }
-            else if(sum<target)
-            {
-                l++;
-            }
-            else
-            {
-                ans.add(Arrays.asList(nums[i], nums[l], nums[r]));
-                while(l<r && nums[l]==nums[l+1])
-                {
-                    l++;
-                }
-                while(l<r && nums[r]==nums[r-1])
-                {
-                    r--;
-                }
-                r--;
-                l++;
-            }
-        }
     }
 }
