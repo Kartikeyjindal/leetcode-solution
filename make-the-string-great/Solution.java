37cc03752749aa1928272c8daec0fad23885fc74
Kartikey Jindal <142749178+Kartikeyjindal@users.noreply.github.com>
2026-07-10 02:07:37 +0530

    Add java solution for LeetCode: Make The String Great

---
+++ make-the-string-great/Solution.java
@@ -0,0 +1,26 @@
+class Solution 
+{
+    public String makeGood(String s)
+    {
+        Stack<Character> stack=new Stack<>();
+
+        for(char c:s.toCharArray())
+        {
+            if(!stack.isEmpty() && Math.abs(c-stack.peek())==32)
+            {
+                stack.pop();
+            }
+            else
+            {
+                stack.push(c);
+            }
+        }
+
+        StringBuilder sb=new StringBuilder();
+        while(!stack.isEmpty())
+        {
+            sb.insert(0,stack.pop());
+        }
+        return sb.toString();
+    }
+}
