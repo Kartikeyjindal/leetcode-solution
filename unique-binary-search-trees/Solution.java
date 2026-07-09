f0405adb3fc6bbe522c3712f60f01d03b0b783ac
Kartikey Jindal <142749178+Kartikeyjindal@users.noreply.github.com>
2026-07-10 02:02:10 +0530

    Add java solution for LeetCode: Unique Binary Search Trees

---
+++ unique-binary-search-trees/Solution.java
@@ -0,0 +1,22 @@
+class Solution 
+{
+    public int numTrees(int n) 
+    {
+        int []dp=new int[n+1];
+        for(int i=0;i<=n;i++)
+        {
+            dp[i]=1;
+        }
+        
+        for(int i=2;i<=n;i++)    
+        {
+            int total=0;
+            for(int j=1;j<=i;j++)
+            {
+               total+=(dp[j-1]*dp[i-j]);
+            }
+            dp[i]=total;
+        }
+        return dp[n];
+    }
+}
