0c19111151c0942dfc093cfffbbfa77d97af6375
Kartikey Jindal <142749178+Kartikeyjindal@users.noreply.github.com>
2026-07-10 01:42:37 +0530

    Add java solution for LeetCode: Reverse Odd Levels Of Binary Tree

---
+++ reverse-odd-levels-of-binary-tree/Solution.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution 
+{
+    public TreeNode reverseOddLevels(TreeNode root) 
+    {
+        solve(root.left,root.right,1);
+        return root;
+    }
+
+    public void solve(TreeNode root1,TreeNode root2,int level)
+    {
+        if(root1==null)
+        {
+            return;
+        }
+        if(root2==null)
+        {
+            return;
+        }
+        if(level%2!=0)
+        {
+            int temp=root1.val;
+            root1.val=root2.val;
+            root2.val=temp;
+        }
+
+        solve(root1.left,root2.right,level+1);
+        solve(root1.right,root2.left,level+1);
+    }
+}
