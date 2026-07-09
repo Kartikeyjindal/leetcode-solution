41481356cef51073377121a049f2fee9481cf27f
Kartikey Jindal <142749178+Kartikeyjindal@users.noreply.github.com>
2026-07-10 01:37:31 +0530

    Add java solution for LeetCode: Check If Binary String Has At Most One Segment Of Ones

---
+++ check-if-binary-string-has-at-most-one-segment-of-ones/Solution.java
@@ -0,0 +1,19 @@
+class Solution 
+{
+    public boolean checkOnesSegment(String s) 
+    {
+       int n=s.length();
+       if(n==1)
+       {
+        return true;
+       }
+       for(int i=1;i<n;i++)
+       {
+            if(s.charAt(i)=='1' && s.charAt(i-1)=='0')
+            {
+                return false;
+            }
+       } 
+       return true;
+    }
+}
