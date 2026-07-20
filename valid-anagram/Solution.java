9d655e02ed7a79568b0401e02744331023ce04fd
Kartikey Jindal <142749178+Kartikeyjindal@users.noreply.github.com>
2026-07-20 19:08:00 +0530

    Add java solution for LeetCode: Valid Anagram

---
+++ valid-anagram/Solution.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution 
+{
+    public boolean isAnagram(String s, String t) 
+    {
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+        
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+        
+        return Arrays.equals(sChars, tChars);
+    }
+}
