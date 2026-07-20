b4fc1746b671606962fd8b1d576d7cf81ec0e675
Kartikey Jindal <142749178+Kartikeyjindal@users.noreply.github.com>
2026-07-20 18:54:12 +0530

    Add java solution for LeetCode: Even Number Of Knight Moves

---
+++ even-number-of-knight-moves/Solution.java
@@ -0,0 +1,10 @@
+class Solution 
+{
+    public boolean canReach(int[] start, int[] target) 
+    {
+        int startcolor=(start[0]+start[1])%2;
+        int endcolor=(target[0]+target[1])%2;
+
+        return startcolor==endcolor;
+    }
+}
