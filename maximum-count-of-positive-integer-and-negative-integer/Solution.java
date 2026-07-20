2454f4f9e6a669053228a9ae151eef0313a4333b
Kartikey Jindal <142749178+Kartikeyjindal@users.noreply.github.com>
2026-07-20 19:03:18 +0530

    Add java solution for LeetCode: Maximum Count Of Positive Integer And Negative Integer

---
+++ maximum-count-of-positive-integer-and-negative-integer/Solution.java
@@ -0,0 +1,30 @@
+class Solution 
+{
+    public int maximumCount(int[] nums) 
+    {
+        int negcount=binarysearch(nums,0);
+        int poscount=nums.length-binarysearch(nums,1);
+        return Math.max(negcount,poscount);
+    }
+
+    private int binarysearch(int []nums,int target)
+    {
+        int left=0;int right=nums.length-1;
+        int result=nums.length;
+
+        while(left<=right)
+        {
+            int mid=(left+right)/2;
+            if(nums[mid] <target)
+            {
+                left=mid+1;
+            }
+            else
+            {
+                result=mid;
+                right=mid-1;
+            }
+        }
+        return result;
+    }
+}
