4c3270fd0c387915d7d667352ac7c19df5493455
Kartikey Jindal <142749178+Kartikeyjindal@users.noreply.github.com>
2026-07-20 19:03:54 +0530

    Add java solution for LeetCode: Apply Operations To An Array

---
+++ apply-operations-to-an-array/Solution.java
@@ -0,0 +1,30 @@
+class Solution 
+{
+    public int[] applyOperations(int[] nums) 
+    {
+        int n=nums.length;
+        for(int i=0;i<n-1;i++)
+        {
+            if(nums[i] == nums[i+1])
+            {
+                nums[i]*=2;
+                nums[i+1]=0;
+            }
+        }
+
+        int nonzero=0;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i] != 0)
+            {
+                nums[nonzero++]=nums[i];
+            }
+        }
+
+        while(nonzero<n)
+        {
+            nums[nonzero++]=0;
+        }
+        return nums;
+    }
+}
