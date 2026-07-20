2cb5f267693eb9dcca5c648a0ed4cff332c826ad
Kartikey Jindal <142749178+Kartikeyjindal@users.noreply.github.com>
2026-07-20 19:05:00 +0530

    Add java solution for LeetCode: Construct Binary Search Tree From Preorder Traversal

---
+++ construct-binary-search-tree-from-preorder-traversal/Solution.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution 
+{
+    public TreeNode bstFromPreorder(int[] preorder) 
+    {
+        TreeNode root=null;
+        for(int n:preorder)
+        {
+            root=insert(root,n);
+        }
+        return root;
+    }
+
+    public TreeNode insert(TreeNode root,int val)
+    {
+        if(root == null)
+        {
+            return new TreeNode(val);
+        }
+        if(root.val>val)
+        {
+            root.left=insert(root.left,val);
+        }
+        else
+        {
+            root.right=insert(root.right,val);
+        }
+        return root;
+    }
+}
