23ca23e6aa021f8e54f904e4dbedc9483b269075
Kartikey Jindal <142749178+Kartikeyjindal@users.noreply.github.com>
2026-07-20 18:54:21 +0530

    Update java solution for LeetCode: Longest Common Subsequence

--- longest-common-subsequence/Solution.java
+++ longest-common-subsequence/Solution.java
@@ -1,30 +1,28 @@
 class Solution 
 {
-    public int [][]temp=new int[1001][1001];
+    int [][]dp;
+    int m ,n;
     public int longestCommonSubsequence(String text1, String text2) 
     {
-        for (int[] r : temp)
-            Arrays.fill(r, -1);
-       return solve(text1,text2,0,0);
-    }
+        m=text1.length();
+        n=text2.length();
 
-    public int solve(String s1,String s2,int i,int j)
-    {
-        if(temp[i][j]!=-1)
+        dp=new int[m+1][n+1];
+
+        for(int i=1;i<=m;i++)
         {
-            return temp[i][j];
+            for(int j=1;j<=n;j++)
+            {
+                if(text1.charAt(i-1)==text2.charAt(j-1))
+                {
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
         }
-        if(i>= s1.length() || j>=s2.length())
-        {
-            return 0;
-        }
-        if(s1.charAt(i)== s2.charAt(j))
-        {
-            return temp[i][j]=1+solve(s1,s2,i+1,j+1);
-        }
-        else
-        {
-            return temp[i][j]=Math.max(solve(s1,s2,i+1,j),solve(s1,s2,i,j+1));
-        }
+        return dp[m][n];
     }
 }
