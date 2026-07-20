a767ae93cb855c73a379747514095047912a2941
Kartikey Jindal <142749178+Kartikeyjindal@users.noreply.github.com>
2026-07-20 19:09:08 +0530

    Add java solution for LeetCode: Kids With The Greatest Number Of Candies

---
+++ kids-with-the-greatest-number-of-candies/Solution.java
@@ -0,0 +1,29 @@
+class Solution 
+{
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) 
+    {
+        int n=candies.length;
+        int max=0;
+        for(int i=0;i<n;i++)
+        {
+            if(candies[i] > max)
+            {
+                max=candies[i];
+            }
+        }
+        ArrayList<Boolean> list=new ArrayList<>();
+
+        for(int i=0;i<n;i++)
+        {
+            if(candies[i] + extraCandies >= max)
+            {
+                list.add(true);
+            }
+            else
+            {
+                list.add(false);
+            }
+        }
+        return list;
+    }
+}
